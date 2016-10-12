b697766f4a53f6199682dc0acc6162cddf1235af
tonystars <danecpipec@gmail.com>
2016-10-12 22:42:59 +0300

    работа с map и тд

--- site/assets/js/google-map-custom.js
+++ site/assets/js/google-map-custom.js
@@ -8,23 +8,23 @@
 //*********************Google MAP *********************/
 		
 		var color = "#9E2811" // google map background colour
-		var saturation = 100 // 
-		var mapLatitude=40.710892 
-		var mapLongitude=-74.004920//(Fist Value Latitude, Second Value ), get YOUR coordenates here!: http://itouchmap.com/latlong.html
-		var mapZoom_value=16 // Map zoom level parameter only numeric  
+		var saturation = 112 //
+		var mapLatitude=53.1895905
+		var mapLongitude=50.09131372//(Fist Value Latitude, Second Value ), get YOUR coordenates here!: http://itouchmap.com/latlong.html
+		var mapZoom_value=17 // Map zoom level parameter only numeric
 
 
-// Google map marker example 2 locations 
+// Google map marker example 2 locations
 		//map-marker #1
-		var marker1_Latitude=40.707892 
-		var marker1_Longitude=-74.008920
-		var marker1_content="<h2>Mathew</h2> Bridge Water, NJ" // marker or  on click content (Info Window) 
+		var marker1_Latitude=53.1898015
+		var marker1_Longitude=50.0896238
+		var marker1_content="<h2>Блинари, кафе-бистро</h2> Самарское область, Россия" // marker or  on click content (Info Window)
 		var marker1_pointerUrl = 'assets/img/map-marker.png' // set your color pointer here!
 
 		//map-marker #2
-		var marker2_Latitude=40.710892
-		var marker2_Longitude=-74.012920
-		var marker2_content="<h2>Mathew</h2>San Diego California" // marker or  on click content (Info Window) 
+		var marker2_Latitude=53.23315402
+		var marker2_Longitude=50.18831328
+		var marker2_content="<h2>Блинари, кафе-бистро</h2>Самарское область, Россия" // marker or  on click content (Info Window)
 		var marker2_pointerUrl = 'assets/img/map-marker.png' // set your color pointer here!
 
 //********************* Google MAP END *********************/
